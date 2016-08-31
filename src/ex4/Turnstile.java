59d3ee817d35d5e0ab9249c486a22794fcdd81f4
Daniel Winkel <Danielwinkel5@hotmail.com>
2016-08-31 19:59:22 +0200

    First Commit

---
+++ src/ex4/Turnstile.java
@@ -0,0 +1,28 @@
+package ex4;
+
+public class Turnstile implements Runnable {
+
+    private final TurnstileCounter counter;
+    private int count;
+
+    final int COUNT_MAX = 1000;
+
+    Turnstile(TurnstileCounter c) {
+        counter = c;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void run() {
+        synchronized (counter) {
+            for (int i = 0; i < COUNT_MAX; i++) {
+                counter.incr();
+                count++;
+            }
+        }
+    }
+
+}
